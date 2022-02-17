4768f15c37129f90bbd53e9d15c1504e5c770c0e
Florian Dupuis <floriandupuis1996@gmail.com>
2022-02-17 13:59:32 +0100

    Implement TypeORM and PostgeSQL connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentModule } from 'src/student/student.module';
 import { TeacherModule } from 'src/teacher/teacher.module';
+import { config } from '../orm.config';
 
 @Module({
-  imports: [TeacherModule, StudentModule],
+  imports: [TypeOrmModule.forRoot(config), TeacherModule, StudentModule],
 })
 export class AppModule {}
